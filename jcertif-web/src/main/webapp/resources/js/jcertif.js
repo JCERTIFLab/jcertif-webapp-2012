8a7bd0ed3af735ce56e6e52544fea30ec994f089
Rossi Oddet <rossi.oddet@gmail.com>
2012-02-07 21:43:27 +0000

    [ACCUEIL] Le bloc "Actualités" contient désormais les derniers tweets de Max
    git-svn-id: https://jcertif-web-app.googlecode.com/svn/trunk@16 bdc9bba0-42ee-03f9-1e93-cd8d94170fb4

--- jcertif-web/src/main/webapp/resources/js/jcertif.js
+++ jcertif-web/src/main/webapp/resources/js/jcertif.js
@@ -1,4 +1,39 @@
-// Header
-function menuOnMouseOver(){
-	
+
+// Update news panel
+function updateNews() {
+
+	$.ajax({
+				url : "http://twitter.com/statuses/user_timeline/bonbhel.json?callback=twitterCallback&count=4",
+				dataType : 'JSONP', // for cross domain
+				error : function() {
+					
+				},
+			});
+
+}
+
+// Callback method for twitter
+function twitterCallback(data, textStatus) {
+	$.each(data, function(i, tweet) {
+		var text = tweet.text;
+		
+		$('#news').append('<div class="home-news-text">' + linkify(text) + '</div><br/>');
+	});
+}
+
+// Add link (<a></a>) for text 'http://..."
+function linkify(text){
+    if (text) {
+        text = text.replace(
+            /((https?\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,
+            function(url){
+                var full_url = url;
+                if (!full_url.match('^https?:\/\/')) {
+                    full_url = 'http://' + full_url;
+                }
+                return '<a href="' + full_url + '" target="_blank">' + url + '</a>';
+            }
+        );
+    }
+    return text;
 }
