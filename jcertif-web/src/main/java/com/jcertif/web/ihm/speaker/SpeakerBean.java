aac862eb67dd41457f12c561e8d0946e7df3ed25
mngdiouf@gmail.com <mngdiouf@gmail.com@bdc9bba0-42ee-03f9-1e93-cd8d94170fb4>
2012-02-18 14:23:15 +0000

    Ajout de la liste des speakers
    git-svn-id: https://jcertif-web-app.googlecode.com/svn/trunk@52 bdc9bba0-42ee-03f9-1e93-cd8d94170fb4

---
+++ jcertif-web/src/main/java/com/jcertif/web/ihm/speaker/SpeakerBean.java
@@ -0,0 +1,52 @@
+package com.jcertif.web.ihm.speaker;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.jcertif.web.model.Speaker;
+import com.jcertif.web.service.ReferentielService;
+import com.jcertif.web.service.ResourceService;
+import com.jcertif.web.service.RestService;
+
+/**
+ * SpeakerBean
+ * @author Mamadou
+ *
+ */
+@Named
+@RequestScoped
+public class SpeakerBean {
+
+	@Inject
+	private ResourceService resService;
+
+	/** REST Web Service **/
+	@Inject
+	private RestService restService;
+
+	/** Referentiel Service **/
+	@Inject
+	private ReferentielService referentielService;
+
+	/**
+	 * A default constructor.
+	 */
+	public SpeakerBean() {
+		super();
+	}
+
+	/**
+	 * @return the speakers
+	 */
+	public List<Speaker> getSpeakers() {
+		return referentielService.getSpeakers();
+	}
+
+	public String getSpeakerPhotoUrl() {
+		return resService.getSpeakerPhotoUrl();
+	}
+
+}
---
+++ jcertif-web/src/main/java/com/jcertif/web/model/Speaker.java
@@ -0,0 +1,113 @@
+package com.jcertif.web.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+/**
+ * Classe Speaker
+ * @author Mamadou
+ *
+ */
+@XmlRootElement
+public class Speaker  {
+
+	private Long id;
+
+	private String prenom;
+
+	private String nom;
+	
+	private String photo;
+
+	private String bio;
+	
+	private String compagnie;
+
+	
+	/**
+	 * @return the compagnie
+	 */
+	public String getCompagnie() {
+		return compagnie;
+	}
+
+	/**
+	 * @param compagnie the compagnie to set
+	 */
+	public void setCompagnie(String compagnie) {
+		this.compagnie = compagnie;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the photo
+	 */
+	public String getPhoto() {
+		return photo;
+	}
+
+	/**
+	 * @param photo the photo to set
+	 */
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	/**
+	 * @return the bio
+	 */
+	public String getBio() {
+		return bio;
+	}
+
+	/**
+	 * @param bio the bio to set
+	 */
+	public void setBio(String bio) {
+		this.bio = bio;
+	}
+
+
+
+}
