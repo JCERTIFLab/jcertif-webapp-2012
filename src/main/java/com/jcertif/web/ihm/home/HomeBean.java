60837b2d4af354ffaa902360115a8bd476be407f
Rossi Oddet <rossi.oddet@gmail.com>
2012-06-13 22:50:01 +0200

    [HOME] Ajout d'une zone éditorial et optimisation des photos pour les améliorer les performances

--- src/main/java/com/jcertif/web/ihm/home/HomeBean.java
+++ src/main/java/com/jcertif/web/ihm/home/HomeBean.java
@@ -28,34 +28,12 @@ public class HomeBean {
 	@Inject
 	private ResourceService resourceService;
 
-	/**
-	 * @return images location for conference
-	 */
-	public List<String> getImagesConference() {
-		return buildPhotosList(resourceService.getPhotoConfEvent2011Url(),NB_PHOTOS_MAX);
+	public String getImageConference() {
+		return "https://lh3.googleusercontent.com/-VDSRjJXi_W0/TnG04Q1UgBI/AAAAAAAACzk/v_ABAZaCZU8/w373-h279-n-k/IMG_0829.JPG";
 	}
 
-	private List<String> buildPhotosList(String baseUrl, int max) {
-		List<String> photos = new ArrayList<String>();
-		
-		for (int i = 0; i < max; i++) {
-			photos.add(baseUrl + "/" + formatFileName(i, NB_DIGIT) + PHOTO_EXT);
-		}
-
-		return photos;
-	}
-	
-	private String formatFileName(final int indice, final int nbDigit) {
-		String formattedFileName = String.valueOf(indice);
-		while(formattedFileName.length() < nbDigit) formattedFileName = "0" + formattedFileName;
-		return formattedFileName;
-	}
-
-	/**
-	 * @return images location for university
-	 */
-	public List<String> getImagesUniversity() {
-		return buildPhotosList(resourceService.getPhotoUnivEvent2011Url(),NB_PHOTOS_MAX);
+	public String getImageUniversity() {
+		return "https://lh3.googleusercontent.com/-vnTHYTPdJqg/TnQ7X9SDuNI/AAAAAAAADKU/JlPmJHwwpsM/w373-h249-n-k/DSC_0233.JPG";
 
 	}
 
